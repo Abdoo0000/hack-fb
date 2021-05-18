ad4253094f0506d98f9cce0952be3e5a5e39ee49
Abdoo0000 <70906199+Abdoo0000@users.noreply.github.com>
2021-05-18 23:10:09 +0200

    Create START.py

---
+++ START.py
@@ -0,0 +1,49 @@
+import time 
+import os
+os.system("clear")
+import webbrowser
+
+print ("\033[0;31m                   ,.")
+print ("\033[0;32m                 ,   `.")
+print ("\033[0;33m               ,  _<>_ `.")
+print ("\033[0;34m             , .- ____`-.`.")
+print ("\033[0;35m           , _.-      ``-._`.")
+print ("\033[0;36m         , ,       /\      `.`.")
+print ("\033[0;37m       ,  /.._  O /  \ O  _.,\ `.")
+print ("\033[0;38m     , / /  \ ``-;.--.:-   /  \ \`.")
+print ("\033[0;39m   ,  : :    \  /\`., /\  /    : : `.")
+print ("\033[0;91m  < <>| |    LWES ELTASA hack :-.\:..>")
+print ("\033[0;32m   `. : :    /  \/, `.\/  \    ; ; ,") 
+print ("\033[0;33m     `.\ \  /_..-:`-- ;-.._\  / /") 
+print ("\033[0;34m       `. \`    O \  / O   ` / ,") 
+print ("\033[0;35m         `.`._     \/     _, ,") 
+print ("\033[0;36m           `..``-.____.-  ,,") 
+print ("\033[0;37m             `.`-.____.- ,") 
+print ("\033[0;38m               `.  <>  ,") 
+print ("\033[0;39m                 `.  ,")  
+print ("\033[0;34m(1) : YouTube Channel")
+print("(2) : Start")
+
+x = "\033[0;31m"
+z = "\033[0;32m"
+v = "\033[0;34m"
+d = input("\033[0;91myour choise » ")
+print (d)
+k = 0
+
+while d !=2 :
+    while k <= 1 :
+        webbrowser.open("https://youtube.com/channel/UCugYk-edxlAeiB7F66Af2Dg")
+else :
+    m = 201283684615
+    l = 0
+    o = "»"
+    n = ("user&password")
+    time.sleep(2)
+    print("\033[0;31mloding ...")
+    time.sleep(2)
+    while m <= 9999999999999 :
+        l=l+1
+        m=m+12
+        print (z,n,l,o,m)
+        time.sleep(0.1)
